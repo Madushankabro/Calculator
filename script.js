48ec89702cc3011e6c3379b336bb82d3de189e78
MADUSHANKA <87461087+Madushankabro@users.noreply.github.com>
2021-09-22 18:25:04 +0530

    script.js(#java_script)

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+let outputScreen = document.getElementById("output-screen");
 
+        function display(num){
+            outputScreen.value += num;
+        }
+        function Calculate(){
+            try{
+                outputScreen.value = eval(outputScreen.value);
+            }
+            catch(err){
+                alart("invalid")
+            }
+        }
+        function Clear(){
+            outputScreen.value = "";
+        }
+        function del(){
+            outputScreen.value = outputScreen.value.slice(0, -1)
+        }
